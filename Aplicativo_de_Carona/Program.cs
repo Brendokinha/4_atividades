409b61d2fbfcbece7054368f1cd6344488162e45
Brenda <brenda.b.santos@edu.senai.br>
2026-04-15 16:09:25 -0300

    feat: Aplicativo de Carona

---
+++ Aplicativo_de_Carona/Program.cs
@@ -0,0 +1,18 @@
+﻿// perguntar o preço da corrida
+Console.Write("Qual é o valor da corrida cralho? : ");
+double preco = double.Parse(Console.ReadLine());
+
+// perguntar o saldo do cliente
+Console.Write("qual é teu saldo nessa caralha pobre fudido? :");
+int saldo = int.Parse(Console.ReadLine());
+
+//logica fudida
+if (saldo >=preco)
+{
+    Console.WriteLine("boa corrida amore! motorista esta a caminho!");
+}
+
+else
+{
+    Console.WriteLine("saldo insuficiente, recarregue pobrezão");
+}
